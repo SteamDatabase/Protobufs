d1c88d69a43891927341d86b9973d3999f3c81fa
Wendy <freeman@steamdb.info>
2017-10-15 10:20:27 +0000

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+Automatically tracked protobufs.
---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rsync -arhv ../steamtrack/Protobufs/ ./steam/
+rsync -arhv ../backend/files/csgo/Protobufs/ ./csgo/
+rsync -arhv ../backend/files/dota/Protobufs/ ./dota2/
+rsync -arhv ../backend/files/tf/Protobufs/ ./tf2/
+
+git add -A
+#git commit -S -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g')"
+#git push
