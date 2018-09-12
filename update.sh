c224902f9fde06bb780d81a3d0be7bd7550b82fc
Wendy <freeman@steamdb.info>
2018-09-12 12:04:12 +0000

    1 files | M  update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ rsync -arhv ../steamtrack/Protobufs/ ./steam/
 rsync -arhv ../backend/files/csgo/Protobufs/ ./csgo/
 rsync -arhv ../backend/files/dota/Protobufs/ ./dota2/
 rsync -arhv ../backend/files/tf/Protobufs/ ./tf2/
+rsync -arhv ../backend/files/artifact/Protobufs/ ./artifact/
 
 git add -A
 git commit -S -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g')"
