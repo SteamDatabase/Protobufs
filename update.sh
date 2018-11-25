97f0ace725e678285e21a4ee7ff8036801ed508c
Wendy <freeman@steamdb.info>
2018-11-25 17:39:14 +0000

    3 files | M  .gitignore, A  google/protobuf/descriptor.proto, M  update.sh

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ rsync -arhv ../backend/files/csgo/Protobufs/ ./csgo/
 rsync -arhv ../backend/files/dota/Protobufs/ ./dota2/
 rsync -arhv ../backend/files/tf/Protobufs/ ./tf2/
 rsync -arhv ../backend/files/artifact/Protobufs/ ./artifact/
+cp -r ./steam/google ./
 
 git add -A
 git commit -S -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g')"
